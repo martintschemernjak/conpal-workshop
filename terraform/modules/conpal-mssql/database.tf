f7fff7729acaa57a4616a3e87c022804d69cb0e1
mtschemernjak <martin.tschemernjak@cloudflight.io>
2022-11-06 08:34:45 +0100

    add group1

--- terraform/modules/conpal-mssql/database.tf
+++ terraform/modules/conpal-mssql/database.tf
@@ -10,12 +10,3 @@ resource "azurerm_mssql_server" "main" {
   public_network_access_enabled = var.public_network_access_enabled
 }
 
-resource "azurerm_mssql_database" "main" {
-  count = length(var.databases)
-
-  name        = var.databases[count.index].name
-  server_id   = azurerm_mssql_server.main.id
-  collation   = var.databases[count.index].collation != null ? var.databases[count.index].collation : var.database_default_collation
-  max_size_gb = var.databases[count.index].max_size_gb != null ? var.databases[count.index].max_size_gb : var.database_default_size
-  sku_name    = var.databases[count.index].sku_name != null ? var.databases[count.index].sku_name : var.database_sku_name
-}
--- terraform/modules/conpal-mssql/secrets.tf
+++ terraform/modules/conpal-mssql/secrets.tf
@@ -10,29 +10,6 @@ resource "random_password" "database_admin_password" {
   special = true
 }
 
-resource "random_string" "mssql_user_database_username" {
-  count   = length(var.databases)
-  length  = 16
-  special = false
-  number  = false
-  upper   = false
-}
-
-resource "random_password" "mssql_user_database_password" {
-  count            = length(var.databases)
-  length           = 16
-  special          = true
-  lower            = true
-  numeric          = true
-  upper            = true
-  min_lower        = 1
-  min_upper        = 1
-  min_numeric      = 1
-  min_special      = 1
-  override_special = ".-_!#"
-}
-
-
 
 resource "azurerm_key_vault_secret" "database_admin_username" {
   name         = "MSSQLAdminUser"
@@ -46,17 +23,3 @@ resource "azurerm_key_vault_secret" "database_admin_password" {
   key_vault_id = var.key_vault.id
 }
 
-
-resource "azurerm_key_vault_secret" "database_username" {
-  count        = length(var.databases)
-  name         = "db-username-${var.databases[count.index].name}"
-  value        = random_string.mssql_user_database_username[count.index].result
-  key_vault_id = var.key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "database_password" {
-  count        = length(var.databases)
-  name         = "db-password-${var.databases[count.index].name}"
-  value        = random_password.mssql_user_database_password[count.index].result
-  key_vault_id = var.key_vault.id
-}
